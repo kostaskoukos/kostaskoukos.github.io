d08fd51204adf1fc7762b6477f2689eaf2c9364b
kostaskoukos <kkonst2014@gmail.com>
2024-09-11 15:21:34 +0300

    refactor: add base.css for tailwind colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    colors: {
+      text: "var(--text)",
+      background: "var(--background)",
+      primary: "var(--primary)",
+      secondary: "var(--secondary)",
+      accent: "var(--accent)",
+    },
+
+    extend: {},
+  },
+  plugins: [],
+};
