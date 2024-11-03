df29d355d370f85840c982623186413a82ab54a3
kostaskoukos <kkonst2014@gmail.com>
2024-11-03 16:28:52 +0200

    refactor: cleanup prose styles

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,17 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            "--tw-prose-body": "var(--text)",
+            "--tw-prose-bold": "var(--text)",
+            "--tw-prose-headings": "var(--text)",
+            "--tw-prose-code": "var(--text)",
+            "--tw-prose-links": "var(--text)",
+          },
+        },
+      },
       colors: {
         text: "var(--text)",
         background: "var(--background)",
