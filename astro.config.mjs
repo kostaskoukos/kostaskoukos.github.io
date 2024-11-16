2271059803563084d8182f4bbdd29f504bf1442b
kostaskoukos <kkonst2014@gmail.com>
2024-11-16 19:35:06 +0200

    feat: add katex with remark/rehype md plugins

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,16 @@
 import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://kostaskoukos.github.io",
   integrations: [tailwind({ applyBaseStyles: true })],
   markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [[rehypeKatex, { displayMode: true }]],
     shikiConfig: {
       themes: {
         dark: "github-dark-default",
